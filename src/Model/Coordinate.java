9a952bdaff7ebb4a3b432c3d55afbff25247430d
MH-L <922lmh922@gmail.com>
2015-10-25 03:37:30 +0800

    added some model classes.

---
+++ src/Model/Coordinate.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Coordinate {
+	public int x;
+	public int y;
+	public Coordinate(int y, int x) {
+
+	}
+}
---
+++ src/application/Client.java
@@ -0,0 +1,12 @@
+package application;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Client {
+	private JFrame frame = new JFrame("Gomoku");
+	private JLabel msgPanel = new JLabel("");
+	private ImageIcon icon;
+	private ImageIcon opponentIcon;
+}
