9a5b065c67ad8f4cd9907f5fa9ca592532ba0fad
MH-L <922lmh922@gmail.com>
2015-10-26 05:56:56 +0800

    Made some GUI stuff.

---
+++ src/application/Game.java
@@ -0,0 +1,15 @@
+package application;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Game {
+	JPanel boardPanel;
+	JFrame mainFrame;
+	JButton btn;
+
+	public Game() {
+
+	}
+}
