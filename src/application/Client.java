f1db3d15cbcd040534ef682d28e5bfbbf4d11709
MH-L <922lmh922@gmail.com>
2015-10-26 06:53:05 +0800

    Now the frame can be closed.

--- src/application/Client.java
+++ src/application/Client.java
@@ -3,10 +3,11 @@ package application;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Client {
 	private JFrame frame = new JFrame("Gomoku");
-	private JLabel msgPanel = new JLabel("");
+	private JPanel msgPanel = new JPanel();
 	private ImageIcon icon;
 	private ImageIcon opponentIcon;
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -2,6 +2,9 @@ package application;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,11 +16,13 @@ public class Main {
 		displayWelcomeFrame();
 	}
 
+	/**
+	 * Displays the welcome frame on game startup.
+	 */
 	private static void displayWelcomeFrame() {
 		JFrame frame = new JFrame("Gomoku Plus");
 		frame.setSize(1000, 600);
 		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel btnPanel = new JPanel();
 		frame.add(btnPanel);
 		JButton singleplayerBtn = getPlainLookbtn(
@@ -29,8 +34,24 @@ public class Main {
 		btnPanel.add(singleplayerBtn);
 		btnPanel.add(multiplayerBtn);
 		btnPanel.add(networkBtn);
+		singleplayerBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				game = new SingleplayerGame(4);
+				((JFrame) singleplayerBtn.getTopLevelAncestor()).dispose();
+			}
+		});
 	}
 
+	/**
+	 * Used to create a plain-look button.
+	 * @param displayText Text to be displayed on the button.
+	 * @param font Font of text on the button.
+	 * @param fontSize Size of font.
+	 * @param fontStyle Bold, regular, light or italic.
+	 * @param color Color of background of the button.
+	 * @return JButton created with specifications.
+	 */
 	protected static JButton getPlainLookbtn(String displayText, String font,
 			int fontSize, int fontStyle, Color color) {
 		JButton btn = new JButton(displayText);
